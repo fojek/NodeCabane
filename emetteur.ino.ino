da5de148f2f8f05e3d7b7b6a179722dea8ce74f8
Marc Fortier <marc.fortier.1@gmail.com>
2022-02-23 21:07:24 -0500

    Update emetteur.ino.ino

--- emetteur.ino.ino
+++ emetteur.ino.ino
@@ -4,38 +4,32 @@
 #include <DallasTemperature.h>
 #include "LowPower.h" // https://github.com/rocketscream/Low-Power
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+// Creation des objets OneWire et sensors
 OneWire oneWire(A0);
-
-// Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
-uint8_t selected;
-
-int counter = 0;
-
-char zone = 'A'; // Changer selon la zone du capteur
-
+// Configuration
+char zone = 'B'; // Changer selon la zone du capteur
 int powerPin = 3;   // La pin qui alimente le MPX4250DP
 int pressionPin = A7; // La sortie du MPX4250DP
 int battPin = A2; // Lecture de la tension de la batterie
 
 // Temps d'hibernation
-class ModeHibernation {
-public:
-  int temps;
-  float temperature;
+const int MODE_HIBERNATION = 900; // 4h = 900 x 8s
+const int MODE_ECONOMIE = 450; // 2h = 450 x 8s
+const int MODE_STANDBY = 112; // 15 minutes = 112 x 8s
+const int MODE_NORMAL = 15; // 2 minutes = 15 x 8s
 
-  ModeHibernation(int t, float T) {
-    this->temps = t;
-    this->temperature = T;
-  }
-};
+// Temperature des modes d'hibernation
+const int TEMPERATURE_ECONOMIE = -8;
+const int TEMPERATURE_STANDBY = -1;
+const int TEMPERATURE_DELTA_HIBERNATION = 7;
 
-ModeHibernation * mOperation = new ModeHibernation(11, -1);  // 11*8s ~= 1.5min
-ModeHibernation * mEconomie = new ModeHibernation(450, -8);  // 450*8s ~= 1h
-ModeHibernation * mHibernation = new ModeHibernation(900, -30);  // 900*8s ~= 2h
+// Niveaux de la batterie
+const float BATTERIE_NIVEAU_CRITIQUE = 5.5;
+const float BATTERIE_NIVEAU_BAS = 6;
 
+// Setup
 void setup() {
   pinMode(powerPin, OUTPUT);
   
@@ -54,6 +48,7 @@ void setup() {
   LoRa.setSignalBandwidth(125E3);
 }
 
+// Fonction pour lire la tension d'alimentation, en millivolts
 long readVcc() {
   long result;
   // Read 1.1V reference against AVcc
@@ -67,16 +62,20 @@ long readVcc() {
   return result;
 }
 
+// Lecture de la tension de la batterie, en volts
 float getBatt() {
   float vcc = readVcc()/1000;
-  return (analogRead(battPin) * vcc / 1024);
+  // 512, à cause du diviseur de tension (x2)
+  return (analogRead(battPin) * vcc / 512);
 }
 
+// Lecture de la temperature
 float getTemp() {
   sensors.requestTemperatures();
   return sensors.getTempCByIndex(0);
 }
 
+// Lecture de la pression
 int getPress() {
   // Cette pin est utilisée pour alimenter le MPX, pour 
   // Économiser de la batterie.
@@ -91,24 +90,67 @@ int getPress() {
   return pression;
 }
 
+// Selection du bon temps d'hibernation selon la situation
 int getModeHibernation(float temperature, float batterie){
-  
-
-  return 10;
+  if(batterie < BATTERIE_NIVEAU_CRITIQUE) {
+    return MODE_HIBERNATION;
+  } else if (batterie < BATTERIE_NIVEAU_BAS) {
+    if(temperature < TEMPERATURE_STANDBY) {
+      return MODE_ECONOMIE;
+    } else {
+      return MODE_STANDBY;
+    }
+  } else {
+    if(temperature < TEMPERATURE_ECONOMIE) {
+      return MODE_ECONOMIE;
+    } else if (temperature < TEMPERATURE_STANDBY) {
+      return MODE_STANDBY;
+    } else {
+      return MODE_NORMAL;
+    }
+  }
 }
 
-void loop() {
+// Met le µC en hibernation le temps requis
+// Verifie si l'hibernation doit être interrompue
+void hibernation(float batt, float tempDepart) {
   
+  int compteur = 0;
+
+  for(int i=0; i<getModeHibernation(tempDepart, batt); ++i){
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    
+    // Chaque 2 minutes,
+    if(++compteur >= 15) {
+      // Si le niveau de la batterie n'est pas critique,
+      if(batt > BATTERIE_NIVEAU_CRITIQUE) {
+        // On vérifie si la température a monté suffisamment
+        // pour avoir besoin d'arrêter l'hibernation
+        if((getTemp()-tempDepart) >= TEMPERATURE_DELTA_HIBERNATION) {
+          return;
+        }
+      }
+      compteur = 0;
+    }
+  }
+}
+
+// Boucle principale
+void loop() {
   // Valeurs
   float tempC = getTemp();
   float batt = getBatt();
-  float pression = getPress();
 
   // Construit le paquet
   LoRa.beginPacket();
   LoRa.print(zone);
   LoRa.print(";");
-  LoRa.print(getPress());
+
+  //Lecture de la pression uniquement si proche de 0°C
+  if(tempC > TEMPERATURE_STANDBY) {
+    LoRa.print(getPress());
+  }
+
   LoRa.print(";");
   LoRa.print(tempC);
   LoRa.print(";");
@@ -116,9 +158,5 @@ void loop() {
   LoRa.print(";");
   LoRa.endPacket();
 
-
-
-  for(int i=0; i<getModeHibernation(tempC, batt); ++i){
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  }
+  hibernation(batt, tempC);
 }
